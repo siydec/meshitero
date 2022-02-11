91ad8e38d7bd81529e73156cd22d09b45517e3fd
siydec <97608879+siydec@users.noreply.github.com>
2022-02-11 11:39:01 +0000

    favorite_end

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,7 @@
 class FavoritesController < ApplicationController
 
   def create
-    @post_image = PostImage.find(params[:post_image_id])
+    post_image = PostImage.find(params[:post_image_id])
     favorite = current_user.favorites.new(post_image_id: post_image.id)
     favorite.save
     redirect_to post_image_path(post_image)
@@ -9,7 +9,7 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
-    @post_image = PostImage.find(params[:post_image_id])
+    post_image = PostImage.find(params[:post_image_id])
     favorite = current_user.favorites.find_by(post_image_id: post_image.id)
     favorite.destroy
     redirect_to post_image_path(post_image)
